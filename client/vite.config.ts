ac685be4a4d86cd3e938f20ae18cf2b59dad41a3
Jose Antonio Amieva <jo_amieva7@hotmail.com>
2024-02-20 18:27:43 -0600

    fixed vite proxy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -54,11 +54,11 @@ export default defineConfig({
     // port: 5173,
     proxy: {
       '/socket.io': {
-        target: 'http://localhost:5000/',
+        target: 'https://avo-pwa.onrender.com/',
         ws: true,
       },
       '/api': {
-        target: 'http://localhost:5000/',
+        target: 'https://avo-pwa.onrender.com/',
         changeOrigin: true,
         secure: false,
         rewrite: path => path.replace(/^\/api/, ''),
