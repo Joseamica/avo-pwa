6b90296187fddca8945fd101a4ec8c60d438f566
Jose Antonio Amieva <jo_amieva7@hotmail.com>
2024-03-04 17:46:37 -0600

    deployment 2 server

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -1,13 +1,23 @@
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
+
+const imageUrls = [
+  'https://firebasestorage.googleapis.com/v0/b/avoqado-d0a24.appspot.com/o/1.%20Madre%20Cafecito%2FMenu%201.png?alt=media&token=3801c756-3f0e-4d42-beaa-bff72812ca18',
+  'https://firebasestorage.googleapis.com/v0/b/avoqado-d0a24.appspot.com/o/1.%20Madre%20Cafecito%2FMenu%202.png?alt=media&token=74a8da94-ad7b-4d7d-8c4a-f2df3bc9ad9e',
+  'https://firebasestorage.googleapis.com/v0/b/avoqado-d0a24.appspot.com/o/1.%20Madre%20Cafecito%2FMenu%203.png?alt=media&token=4732531a-8b2c-4e3f-bd37-19d4710fde8d',
+]
+
 async function main() {
   console.log('🌱 Seeding...')
   console.time(`🌱 Database has been seeded`)
   const venueExist = await prisma.venue.findFirst()
-  if (venueExist) {
-    console.log('🌱 Database has been seeded')
-    return
-  }
+  // if (venueExist) {
+  //   console.log('🌱 Database has been seeded')
+  //   return
+  // }
+  await prisma.menu.deleteMany()
+  await prisma.table.deleteMany()
+  await prisma.venue.deleteMany()
   const venue = await prisma.venue.create({
     data: {
       name: 'Madre Cafecito',
@@ -20,17 +30,19 @@ async function main() {
       tipPercentage1: '0.1',
       tipPercentage2: '0.15',
       tipPercentage3: '0.2',
+      stripeAccountId: 'acct_1NuRFGBAuNoVK1pM',
     },
   })
   const tableNumbers = await createTables(venue.id, 10)
-  // for (let i = 1; i <= 10; i++) {
-  //   await prisma.menu.create({
-  //     data: {
-  //       name: `Menu ${i}`,
-  //       venue: { connect: { id: venue.id } },
-  //     },
-  //   })
-  // }
+  for (let i = 1; i <= 3; i++) {
+    await prisma.menu.create({
+      data: {
+        name: `Menu ${i}`,
+        venue: { connect: { id: venue.id } },
+        imageUrl: imageUrls[i - 1],
+      },
+    })
+  }
   console.timeEnd(`🌱 Database has been seeded`)
 
   console.log({ venue, tableNumbers })
